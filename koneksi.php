cd2ec3b9cea1e8b738d48c1825977e101c7c1bb2
muftiibr-cloud <muftiibrahim342@gmail.com>
2025-12-02 17:19:56 +0700

    coba

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $nama_database = "db_bukutamu";
+    $koneksi = new mysqli($host, $user, $password, $nama_database);
+?>
